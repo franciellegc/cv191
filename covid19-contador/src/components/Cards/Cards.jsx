2080b4b8416f56d4cb3af48dfbdb6a6964e22b11
rodrigodsluz <rodrigodsluz@gmail.com>
2020-09-03 08:16:50 -0300

    Add

---
+++ covid19-contador/src/components/Cards/Cards.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
+
+const Cards = (props) => {
+  console.log(props);
+
+
+  return (
+      <div className={styles.container}>
+        <Grid container spacing={3} justify="center" >
+          <Grid item component={Card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>Infected</Typography>
+              <Typography variant="h5">REAL DATA</Typography>
+              <Typography color="textSecondary">REAL DATE</Typography>
+              <Typography variant="body">Number of actives cases of COVID-19</Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid item component={Card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+              <Typography variant="h5">REAL DATA</Typography>
+              <Typography color="textSecondary">REAL DATE</Typography>
+              <Typography variant="body">Number of recoveries from of COVID-19</Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid item component={Card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+              <Typography variant="h5">REAL DATA</Typography>
+              <Typography color="textSecondary">REAL DATE</Typography>
+              <Typography variant="body">Number of deaths caused by COVID-19</Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      </div>
+    )
+}
+
+export default Cards;
